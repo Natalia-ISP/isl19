6f2bb26aeaa76825d0ef645f6ae61e40a0ce2a0e
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-14 18:07:54 +0300

    Step 6: functions in JavaScript

--- main.js
+++ main.js
@@ -49,10 +49,74 @@
 //     }
 // }
 
-for (let i = 1; i <= 4; i++) {
-    let a = "";
-    for (let j = 1; j <= i; j++){
-        a += "*";
-    }
-    console.log(a);
+// for (let i = 1; i <= 4; i++) {
+//     let a = "";
+//     for (let j = 1; j <= i; j++){
+//         a += "*";
+//     }
+//     console.log(a);
+// }
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(3, 5))
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// console.log(multiply(3, 5))
+
+// function sayHello(name) {
+//     console.log(`hello, ${name}`);
+// }
+// sayHello("vvvv")
+
+// function  printInfo(name, age) {
+//     console.log(`hello ${name}, u ${age}!`);
+// }
+// printInfo("nata", 18);
+
+// function greet(name = "гость") {
+//     console.log("привет, " + name);
+// }
+// greet();
+// greet("anastasia");
+
+// function calculateDiscount(price = 0, discount = 10) {
+//     console.log(`итого: ${price} rub, ${discount}%` );
+// }
+// calculateDiscount();
+// calculateDiscount(1000);
+
+// const add = function (a, b) {
+//     return a + b;
+// };
+// console.log(add(2, 3));
+
+// function makeCounter() {
+//     let count = 0;
+//     return function () {
+//         count++;
+//         return count;
+//     };
+// }
+// const counter = makeCounter()
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+function sumFunc(a, b) {
+    return a + b;
+}
+const sumFunc2 = (a, b) => a + b;
+const double = x => x * 2;
+const sumFunc3 = (a, b) => a + b;
+const sayGreeting = () => console.log("hello");
+const square = x => x * x;
+const calculate = (a, b) => {
+    let result = a + b;
+    return result * 2;
 }
